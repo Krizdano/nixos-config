798debe464f4de3e8fbfbbb0915a40ebead3ddda
krizdano <opensauce.passover167@slmail.me>
2024-11-02 14:46:24 +0530

    Impermanence: added workaround to latest nixos commit that causes problems with persisting /etc/machine-id

--- hosts/persist.nix
+++ hosts/persist.nix
@@ -21,4 +21,11 @@ _: {
       "/etc/machine-id"
     ];
   };
+
+  # The service systemd-machine-id-commit.service will now cause problems
+  # if /etc/machine-id is configured as a bind mount. It may work if it is a symlink.
+  # Merged PR: https://github.com/NixOS/nixpkgs/pull/351151
+  # Issue: https://github.com/nix-community/impermanence/issues/229
+  boot.initrd.systemd.suppressedUnits = [ "systemd-machine-id-commit.service" ];
+  systemd.suppressedSystemUnits = [ "systemd-machine-id-commit.service" ];
 }
