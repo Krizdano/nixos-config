9ba378a6f44679e485e681a5220d85e1671c8d83
krizdano <opensauce.passover167@slmail.me>
2024-11-07 16:25:00 +0530

    emacs: updated to version 30

--- modules/programs/emacs.nix
+++ modules/programs/emacs.nix
@@ -1,7 +1,7 @@
 { pkgs, ...}: {
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs29-pgtk;
+    package = pkgs.emacs30-pgtk;
     extraPackages = epkgs: with epkgs; [
       vterm
       evil
