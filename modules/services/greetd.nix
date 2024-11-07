e3f40fdd75c0e76cecbb6547b294e4c70a5e4bd8
krizdano <opensauce.passover167@slmail.me>
2024-11-07 17:18:13 +0530

    niri: using --session

--- modules/services/greetd.nix
+++ modules/services/greetd.nix
@@ -3,11 +3,11 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --user-menu  --time --cmd niri";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --user-menu  --time --cmd niri --session";
         user = "greeter";
       };
       initial_session = {
-        command = "niri";
+        command = "niri --session";
         user = "${username}";
       };
     };
